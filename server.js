4161bf92afe25f5edaa5574b1fd2da8827b8b5df
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-08-22 09:49:55 -0500

    adds files for controllers and models empty

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+
+require('dotenv').config();
+
+require('')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
